b859ef69c052f760f828dd7f2eb89ad4d3e8a672
Baruselli <barusell@sissa.it>
2017-10-09 17:29:50 +0200

    a

---
+++ lectures/01_intro/hello.cc
@@ -0,0 +1,9 @@
+#include <iostream>			//declarations
+
+int main(){
+
+  int a; 				//declaration, still not defined
+  std::cout << "Hello world!\n";
+  return 7;
+
+}
--- lectures/01_intro/hello_name.cc
+++ lectures/01_intro/hello_name.cc
@@ -3,6 +3,7 @@
 
 int main() {
   std::cout << "Insert your name and age:\n";
+  //"Insert your name and age:\n" >> std::cout; does not work
   std::string name;
   unsigned int age;
   std::cin >> name >> age;
