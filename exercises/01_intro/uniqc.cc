9b9cb5977c65e5fda88d6a2c07b42c7e69ddc685
Baruselli <barusell@sissa.it>
2017-10-09 17:49:04 +0200

    a

--- exercises/01_intro/uniqc.cc
+++ exercises/01_intro/uniqc.cc
@@ -5,14 +5,23 @@ int main(){
 
 std::string line_in;
 std::string line_old="";
+int n=0;
+bool beginning=1;
 
 while(std::getline(std::cin,line_in))
 {
-if (line_in != line_old) 
-  std::cout << line_in << std::endl;
-line_old=line_in;
+if (line_in == line_old)
+ {n++;}
+else  {
+//  if (line_old != "") 
+if (not beginning) std::cout << n << " "<< line_old << std::endl;
+  line_old=line_in;
+  n=1;
+}
+beginning=0;
 }
 
+  std::cout << n << " "<< line_old << std::endl;
 
  return 0;
 
