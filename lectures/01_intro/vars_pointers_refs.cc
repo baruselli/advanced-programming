249f1ba9ba28f2b1e68fda5990688fc382108ed6
Baruselli <barusell@sissa.it>
2017-10-12 16:55:39 +0200

    ex2b

--- lectures/01_intro/vars_pointers_refs.cc
+++ lectures/01_intro/vars_pointers_refs.cc
@@ -4,10 +4,17 @@ int main() {
   double d = 9.9;
   double &ref = d;			//reference (alias) different name for the same object
   double *ptr = &d;			//pointer (it is an object by itself)
+  int i=2;
+  int *ptri = &i;
   std::cout << "content of   d: " << d << std::endl;
   std::cout << "content of ref: " << ref << std::endl;
   std::cout << "content of ptr: " << ptr << std::endl << std::endl;
   std::cout << "address of   d: " << &d << std::endl;
   std::cout << "address of ref: " << &ref << std::endl;
   std::cout << "address of ptr: " << &ptr << std::endl;
+  std::cout << "size of d: " << sizeof(d) << std::endl;
+  std::cout << "size of ref: " << sizeof(ref) << std::endl;
+  std::cout << "size of ptr: " << sizeof(ptr) << std::endl;
+std::cout << "size of i: " << sizeof(i) << std::endl;
+  std::cout << "size of ptri: " << sizeof(ptri) << " (always 8 for 64 bit)"<< std::endl;
 }
