458c91354af21c0cbbb55983667607d3026c51fe
Baruselli <barusell@sissa.it>
2017-10-12 18:18:02 +0200

    prime reverse

--- exercises/02_arrays/reverse.cc
+++ exercises/02_arrays/reverse.cc
@@ -4,8 +4,8 @@
 template <typename num>
 num* init_vector(int len) {
     num* v = new num[len];
-    for (int i =len-1; i > -1; --i)
-        v[i]=0;
+    //for (int i =len-1; i > -1; --i)
+    //    v[i]=0;
     return v;
 }
 
@@ -13,23 +13,29 @@ num* init_vector(int len) {
 template <typename num>
 void print_vector_reverse(const num* vec, const int n) {
 for (int i =n-1; i > -1; --i)
-    std::cout << vec[i] << std::endl;
+    std::cout << vec[i] <<  " ";
+std::cout << std::endl;
 }
 
 
 int main(int argc, char* argv[]) {
     int len;
+
     std::cin>>len;
 
-    
-    double* v= init_vector<int>(len);
- 
+    double* v_a= init_vector<double>(len);
     for (int i=0; i < len; ++i)
-    std::cin >> v[i];   
+        std::cin >> v_a[i];   
+    
+  //  int* v= init_vector<int>(len);
  
-    print_vector_reverse<int>(v,len);
+  //  for (int i=0; i < len; ++i)
+  //      v[i]=atoi(v_a[i]);   
+ 
+    print_vector_reverse(v_a,len);
     
-    
+  delete[](v_a); 
+
     
     
      return 0;
