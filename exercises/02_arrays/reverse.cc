3df0509a4622c99c46026f60006dca76b9dcc624
Baruselli <barusell@sissa.it>
2017-10-12 16:55:39 +0200

    ex2b

--- exercises/02_arrays/reverse.cc
+++ exercises/02_arrays/reverse.cc
@@ -4,10 +4,15 @@
 template <typename num>
 num* init_vector(int len) {
     num* v = new num[len];
+<<<<<<< c23d21341a2312df0f207d66394776caab948192
 
     for (int i =len-1; i > -1; --i)
         v[i]=0;
 //    return v;
+=======
+    for (int i =len-1; i > -1; --i)
+        v[i]=0;
+>>>>>>> ex2b
     return v;
 }
 
@@ -23,11 +28,25 @@ int main(int argc, char* argv[]) {
     int len;
     std::cin>>len;
 
+<<<<<<< c23d21341a2312df0f207d66394776caab948192
     int* v= init_vector<int>(len);
     for (int i=0; i < len; ++i)
     std::cin >> v[i];   
     print_vector_reverse<int>(v,len);
 
 //    v= init_vector<int>(len);
+=======
+    
+    int* v= init_vector<int>(len);
+ 
+    for (int i=0; i < len; ++i)
+    std::cin >> v[i];   
+ 
+    print_vector_reverse<int>(v,len);
+    
+    
+    
+    
+>>>>>>> ex2b
      return 0;
 }
