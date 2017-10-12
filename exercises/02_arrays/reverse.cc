c23d21341a2312df0f207d66394776caab948192
Baruselli <barusell@sissa.it>
2017-10-12 16:43:48 +0200

    ex2

--- exercises/02_arrays/reverse.cc
+++ exercises/02_arrays/reverse.cc
@@ -23,15 +23,11 @@ int main(int argc, char* argv[]) {
     int len;
     std::cin>>len;
 
-    
     int* v= init_vector<int>(len);
- 
     for (int i=0; i < len; ++i)
     std::cin >> v[i];   
- 
     print_vector_reverse<int>(v,len);
-        
-    
-    
+
+//    v= init_vector<int>(len);
      return 0;
 }
